303f84127f741781a9e4ac255f4d18c6c9b8d667
Hannah <hannahmgrout@gmail.com>
2022-04-18 19:51:24 -0700

    commit tree 94a397149a7d43ee42eab062807a573ca6cc51fa

--- main.js
+++ main.js
@@ -4,8 +4,40 @@ const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
 
+  
+  const heartBtns = document.querySelectorAll(".like-glyph")
+  heartBtns.forEach(heartBtn => heartBtn.addEventListener('click', e => handleHeart(heartBtn)))
+ 
 
+ function handleHeart(heartBtn){
+    mimicServerCall()
+    .then( () => {
+    
+      if(heartBtn.textContent === EMPTY_HEART){
+        heartBtn.classList.add('activated-heart')
+        heartBtn.innerHTML = FULL_HEART
+      }
+      else{
+        heartBtn.classList.remove('activated-heart')
+        heartBtn.innerHTML = EMPTY_HEART
+      }
+    })
+  
+    .catch( errorMsg => {
+    const error= document.getElementById('modal')
+    error.classList.remove('hidden')
+    p = document.getElementById('modal-message')
+    p.innerText = `${errorMsg}`
+    error.append(p)
 
+    setTimeout(() =>{
+      const err= document.getElementById('modal')
+      err.classList.add('hidden')
+    }, 3000)
+    })
+  }
+
+  handleHeart()
 
 //------------------------------------------------------------------------------
 // Don't change the code below: this function mocks the server response
